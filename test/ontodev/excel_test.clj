4b1b756d746d77ca33b20034ec2d73035c829a16
James A. Overton <james@overton.ca>
2013-01-03 11:36:52 -0500

    Version 0.2.0 handles all cell values as strings.

--- test/ontodev/excel_test.clj
+++ test/ontodev/excel_test.clj
@@ -2,4 +2,14 @@
   (:use midje.sweet
         ontodev.excel))
 
-(fact "I should add more tests." => truthy)
+(defn check-row
+  [row]
+  (facts "check-row"
+    (:integer row) => "1001" 
+    (:float row) => "1001.01" 
+    (:formula row) => "2002.01"))
+
+(let [workbook (load-workbook "resources/test.xlsx")
+      data     (read-sheet workbook)]
+  (doall (map check-row data)))
+
