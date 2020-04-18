2a3585931292c50f38ea5703e77c6c01596fc1bc
James A. Overton <james@overton.ca>
2020-04-18 10:23:24 -0400

    Bump version to 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ontodev/excel "0.2.4"
+(defproject ontodev/excel "0.2.5"
   :description "A thin Clojure wrapper around a small part of Apache POI for
                 reading .xlsx files."
   :url "http://github.com/ontodev/excel"
